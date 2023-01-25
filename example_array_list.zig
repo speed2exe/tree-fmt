d1e852d8e6e4a5658daad02d8056c3bb884ec130
Fu Zi Xiang <speed2exe@live.com.sg>
2023-01-25 20:28:45 +0800

    refactor components

---
+++ example_array_list.zig
@@ -0,0 +1,29 @@
+const std = @import("std");
+const TreeFormatter = @import("./src/tree_fmt.zig").TreeFormatter;
+
+pub fn main() !void {
+    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+    const allocator = gpa.allocator();
+    defer {
+        const leaked = gpa.deinit();
+        if (leaked) {
+            @panic("leaked memory!");
+        }
+    }
+
+    var w = std.io.getStdOut().writer();
+    var tree_formatter = TreeFormatter.init(allocator, .{
+        .array_print_limit = 5,
+        .print_u8_chars = false,
+    });
+
+    var array_list = std.ArrayList(u8).init(allocator);
+    defer array_list.deinit();
+    
+    var i: u8 = 0;
+    while (i < 100) : (i += 1) {
+        try array_list.append(i);
+    }
+
+    try tree_formatter.formatValue(w, array_list);
+}
---
+++ example_slice.zig
@@ -0,0 +1,33 @@
+const std = @import("std");
+const TreeFormatter = @import("./src/tree_fmt.zig").TreeFormatter;
+
+pub fn main() !void {
+    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+    const allocator = gpa.allocator();
+    defer {
+        const leaked = gpa.deinit();
+        if (leaked) {
+            @panic("leaked memory!");
+        }
+    }
+
+    var w = std.io.getStdOut().writer();
+    var tree_formatter = TreeFormatter.init(allocator, .{
+        .array_print_limit = 1,
+        .print_u8_chars = false,
+    });
+
+    var array_list = std.ArrayList(u8).init(allocator);
+    defer array_list.deinit();
+
+    var i: u8 = 0;
+    while (i < 100) : (i += 1) {
+        try array_list.append(i);
+    }
+
+    try tree_formatter.formatValueWithId(w, array_list, "array");
+
+    try tree_formatter.formatValueWithId(w, std.mem.span(array_list.items[0..0]), "array");
+    try tree_formatter.formatValueWithId(w, std.mem.span(array_list.items[0..1]), "array");
+    try tree_formatter.formatValueWithId(w, std.mem.span(array_list.items[0..2]), "array");
+}
