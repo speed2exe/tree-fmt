375307541c3f6bdbbfc92f2b5d7986e1d8e10a2e
Fu Zi Xiang <speed2exe@live.com.sg>
2023-03-07 23:19:11 +0800

    simplify build.zig

--- build.zig
+++ build.zig
@@ -7,12 +7,5 @@ pub fn build(b: *std.Build) void {
         .target = b.standardTargetOptions(.{}),
         .optimize = b.standardOptimizeOption(.{}),
     });
-
-    const main_tests = b.addTest(.{
-        .root_source_file = .{ .path = "src/example/anon_struct.zig" },
-    });
-    const test_step = b.step("test", "tree pretty fmt");
-    test_step.dependOn(&main_tests.step);
-
     lib.install();
 }
