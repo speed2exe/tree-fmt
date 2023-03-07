3272ae1ac9f01b7daa35faa5d9b2eb3876b438cc
Fu Zi Xiang <speed2exe@live.com.sg>
2023-03-08 00:47:05 +0800

    ignore add module

--- build.zig
+++ build.zig
@@ -1,7 +1,7 @@
 const std = @import("std");
 
 pub fn build(b: *std.Build) void {
-    b.addModule("tree-fmt", .{
+    _ = b.addModule("tree-fmt", .{
         .source_file = std.Build.FileSource.relative("src/tree_fmt.zig"),
     });
 }
